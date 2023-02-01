8bab1c2f80df54c4dfd97d65d58b413e657fd3bb
Daniel T <dot1809@hsutx.edu>
2023-01-31 21:40:59 -0600

    I removed some imports in VideoController that weren't in use

--- src/main/java/com/example/springvue/controllers/VideoController.java
+++ src/main/java/com/example/springvue/controllers/VideoController.java
@@ -1,15 +1,10 @@
 package com.example.springvue.controllers;
 
-import com.example.springvue.models.Friend;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Controller
 /*@RequestMapping() REQUEST MAPPING allows you to change your root directory name from being plain "/"
