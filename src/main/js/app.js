8d886a8ff18e9d3d5bafe15a9626574a0a4c66c6
Daniel T <dot1809@hsutx.edu>
2023-02-09 14:49:45 -0600

    Main changes you need to be concerened about> super.js and app.js \n Other changes: I added 2 new dependenciescalled firebase and firebase-admin, these are what we're goign to sue for our database' In theory, everything is workign correctly now. Inside of super.js, I'll need you to add a config file to path and change the directory stored in firepath to match the directory where that thign is

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,6 +4,36 @@ import axios from 'axios';
 import App from './components/App.vue';
 import routes from './routes';
 import store from './store';
+import ticket from './super';
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCbuK5yf4uAur4qpjQcDmdVbwdgicFfCew",
+    authDomain: "sp23-bcfc0.firebaseapp.com",
+    databaseURL: "https://sp23-bcfc0-default-rtdb.firebaseio.com",
+    projectId: "sp23-bcfc0",
+    storageBucket: "sp23-bcfc0.appspot.com",
+    messagingSenderId: "412407040780",
+    appId: "1:412407040780:web:435c300905b10d21495729",
+    measurementId: "G-P3VXXSRQXZ"
+};
+
+const base=initializeApp(firebaseConfig);
+
+export const db = base.firestore();
+db.collection("stuff").add({
+    "nepal":{
+        "flag":"triangular",
+        "sky": "blue"
+    }
+}).then(data=>console.log(data))
+
+
 
 Vue.use(VueRouter);
 
