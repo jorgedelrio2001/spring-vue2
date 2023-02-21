9e20e4c9ee954de4128857d6cf7cc96cec594f55
jorge.jorge <jorge.jld1901@hsutx.edu>
2023-02-21 12:32:20 -0600

    This class will be interacting with the Firebase database and will be performing more operations once we implement the Firebase Admin SDK.

--- src/main/java/com/example/springvue/controllers/service/FirebaseSerivce.java
+++ src/main/java/com/example/springvue/controllers/service/FirebaseSerivce.java
@@ -1,2 +1,15 @@
-package com.example.springvue.controllers.service;public class FirebaseSerivce {
+package com.example.springvue.controllers.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseService {
+
+    private final FirebaseDatabase database;
+
+    public FirebaseService(FirebaseApp firebaseApp) {
+        this.database = FirebaseDatabase.getInstance(firebaseApp);
+    }
+
+    // TODO: Implement database operations
 }
