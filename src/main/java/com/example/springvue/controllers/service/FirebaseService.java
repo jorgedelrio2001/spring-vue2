7a4ca33237822fd19f1ed838e3f532e97ea58855
Daniel T <dot1809@hsutx.edu>
2023-02-24 16:20:01 -0600

    I commented 2 imports in build.gradle because gradle couldn't find them. I also fixed a Typo in the name of FirebaseService.java cuz of a compile crash. I also set the getDatabase() method in Firebase service to return null so that everythign compiles..

--- src/main/java/com/example/springvue/controllers/service/FirebaseService.java
+++ src/main/java/com/example/springvue/controllers/service/FirebaseService.java
@@ -1,5 +1,7 @@
 package com.example.springvue.controllers.service;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +12,11 @@ public class FirebaseService {
 
     public FirebaseService(FirebaseApp firebaseApp) {
         this.database = FirebaseDatabase.getInstance(firebaseApp);
+    }
+
+    public FirebaseDatabase getDatabase() {
+        //nothing here yet, so I just returned null so that the code doesn't break in compile time
+        return null;
     }
 
     // TODO: Implement database operations
