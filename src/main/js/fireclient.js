95cf5e7e4107164cbc6cd87afa95c516e6417634
jorge.jorge <jorge.jld1901@hsutx.edu>
2023-02-26 13:51:32 -0600

    commented out authentication and login status

--- src/main/js/fireclient.js
+++ src/main/js/fireclient.js
@@ -27,13 +27,13 @@ const firestore=firebaseApp.firestore();
 //authoritah.createUserWithEmailAndPassword("creativedan2@gmail.com","uber");
 /*firestore.collection('funeral homes').doc('FH').update('fhname','epic funeral home')
 firestore.collectionGroup('funeral homes ' )*/
-
+/*
 authoritah.signInWithEmailAndPassword(
     "creativedan2@gmail.com","superepicawesomecool"
 )
     .then((userCreds)=>{console.log("Hello bud")})
     .catch();
-
+*/
 /*authoritah.createUserWithEmailAndPassword("creativedan2@gmail.com","superepicawesomecool")
     .then((userCredential) => {
         // Signed in
@@ -46,7 +46,7 @@ authoritah.signInWithEmailAndPassword(
         const errorMessage = error.message;
         // ..
     });*/
-
+/*
 authoritah.onAuthStateChanged( user=>{
     if(user != null){
         console.log('logged in!')
@@ -56,7 +56,7 @@ authoritah.onAuthStateChanged( user=>{
     }
 })
 
-
+*/
 //export const db = base.firestore();
 //^^^^The code will give us a bit of hope that firebase won't be a pain in the neck any longer
 //DB set-up should be easy
