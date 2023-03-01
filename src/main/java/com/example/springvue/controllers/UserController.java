85749e7dddc7c640c1282aa4f0166865fe886c3e
jorge.jorge <jorge.jld1901@hsutx.edu>
2023-03-01 00:08:37 -0600

    updated user controller

--- src/main/java/com/example/springvue/controllers/UserController.java
+++ src/main/java/com/example/springvue/controllers/UserController.java
@@ -1,37 +1,36 @@
-
 package com.example.springvue.controllers;
 
+import com.example.springvue.models.Address;
+import com.example.springvue.models.User;
+import com.example.springvue.repositories.AddressRepository;
+import com.example.springvue.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-public class UserController { public static void main(String[] args){
-        // Load the Firebase Admin SDK configuration
-           //Inside file input stream immediately below put the name of your credentials file
-       //     FileInputStream serviceAccount = new FileInputStream("C:\\Users\\User\\zer\\adiam.json");
-            /*BufferedReader bf= new BufferedReader(new InputStreamReader(serviceAccount));
-            int i=0;
-            while(bf.ready()){
-                System.out.print(i+":");
-                i++;
-                System.out.println(bf.readLine());
-            } //Use this thing to check that your file is reading....*/
+public class UserController {
 
+    @Lazy
+    @Autowired
+    private UserRepository userRepository;
 
-        //HashMap map= new HashMap<String,String>();
-        //map.put("Oh","no");
+    @Lazy
+    @Autowired
+    private AddressRepository addressRepository;
 
+    @GetMapping(path="/rest/users")
+    public List<User> getUsers() {
+        return this.userRepository.findByOrderByLastNameAsc();
+    }
 
-        /*FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-
-        FirebaseApp firebaseApp= FirebaseApp.initializeApp(options);
-
-       // System.out.print(firestoreOptions);
-
-        //System.out.print(db.collection("funeral homes").add(map)==null);
-        //System.out.print(db==null);
-        */
+    @GetMapping(path="/rest/address/{addressUuid}")
+    public Optional<Address> getUserAddress(@PathVariable String addressUuid) {
+        return this.addressRepository.findById(addressUuid);
     }
 }
