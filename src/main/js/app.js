7250906ddcfbfd380994d695eec908e1c8f1ac71
jorge.jorge <jorge.jld1901@hsutx.edu>
2023-03-01 00:17:09 -0600

    app.js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 import App from './components/App.vue';
 import routes from './routes';
 import store from './store';
-import {logOut} from "./fireclient";
 
-
+Vue.config.devtools = true;
 Vue.use(VueRouter);
+
 export const router = new VueRouter({
     mode: 'history',
     base: '/',
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         router,
         store,
-        logOut,
         render: (h) => h(App),
     }).$mount('#app');
 });
