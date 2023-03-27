a9207c3a4ce8d517aeca7240b47387a81335a564
jorgedelrio2001 <jorgedelrio2001@gmail.com>
2023-03-27 00:05:42 -0500

    added dependencies, Restructured DatabaseConfig and also added hibernate to exceute script. Profiles from DatabaseConfig are managed in application.properties

--- src/main/java/com/example/springvue/config/DatabaseConfig.java
+++ src/main/java/com/example/springvue/config/DatabaseConfig.java
@@ -8,48 +8,67 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @Configuration
 public class DatabaseConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
 
     /*
     Steps to add data source:
-    1) Ad   ded spring-boot-starter-data-jpa (database resources), spring-boot-starter-data-rest (REST resources), and mysql:mysql-connector-java (runtime connector for the mysql database driver)
-    dependencies to the build.gradle file (refresh gradle project afterwards)
-    2) Added datasource properties to the applications.properties file
-    3) Created this class, DatabaseConfig.java, which instantiates configuration beans based on the active spring profile
-    4) Created the models package and added a class which could represent our user model (User.java) and a class which could represent our address model (Address.java)
-    5) Created the repace for interacting with address modelsositories package and added a repository interface for interacting with user models and added a repository interf
-    6) Created the UserController and added a couple of functions which serve as REST endpoints and use our repository classes to pull data from the mysql database
+    Created this class, DatabaseConfig.java, which instantiates configuration beans based on the active spring profile
+    Created the models package and added a class which could represent our user model (User.java) and a class which could represent our address model (Address.java)
+    Created the repace for interacting with address modelsositories package and added a repository interface for interacting with user models and added a repository interf
+    Created the UserController and added a couple of functions which serve as REST endpoints and use our repository classes to pull data from the mysql database
      */
 
     // These spring beans will be instantiated when the spring.profiles.active is set to prd
+    //TO UNDERSTAND PROFILES, CHECK APPLICATION.PROPERTIES.
     @Bean
     @Primary
-    @Profile("prd") //I don't personally know the syntax, but I'm assuming this draws from application.properties
-                    //Specifically the environmental variables that involve prd
-    public DataSource productionDataSource() {
+    @Profile("prd")
+    public DataSource productionDataSource() throws IOException {
+        executeSqlScript();
         return this.productionDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
     }
 
     @Bean
     @Primary
-    @ConfigurationProperties("spring.datasource.prd")//This must also involve itself with production mode: I might be more accurate on this one
+    @Profile("prd")
+    @ConfigurationProperties("spring.datasource.prd")
     public DataSourceProperties productionDataSourceProperties() {
         return new DataSourceProperties();
     }
 
     // These spring beans will be instantiated when the spring.profiles.active is set to tst (running a local data source for now)
     @Bean
-    @Profile("tst")//unsure what "tst" represents
-    public DataSource localDataSource() {
+    @Profile("prd")
+    public DataSource localDataSource() throws IOException {
+        executeSqlScript();
         return this.localDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
     }
 
     @Bean
+    @Profile("local")
     @ConfigurationProperties("spring.datasource.local")
     public DataSourceProperties localDataSourceProperties() {
         return new DataSourceProperties();
+    }
+    @Transactional
+    public void executeSqlScript() throws IOException {
+        String sqlScriptPath = "src/main/resources/data.sql";
+        Path path = Paths.get(sqlScriptPath);
+        String sqlScript = Files.lines(path).collect(Collectors.joining("\n"));
+
+        entityManager.createNativeQuery(sqlScript).executeUpdate();
     }
 }
--- src/main/java/com/example/springvue/repositories/AddressRepository.java
+++ src/main/java/com/example/springvue/repositories/AddressRepository.java
@@ -2,6 +2,7 @@ package com.example.springvue.repositories;
 
 import com.example.springvue.models.Address;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 public interface AddressRepository extends PagingAndSortingRepository<Address, String> {
 
